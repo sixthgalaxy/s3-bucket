40c00bfe916a024647bd74b80a064e086405a09c
sixthgalaxy <superwhiteman@gmail.com>
2022-04-24 13:25:55 +0100

    fmt

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ module "s3_bucket" {
   source                 = "./module/s3"
   environment            = var.environment
   enbable_bucket_version = false
-  bucket-name = var.bucket-name
+  bucket-name            = var.bucket-name
 
 }
