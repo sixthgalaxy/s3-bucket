ada2a961e7f118cc91eb68ad38c350c9a10b08cc
sixthgalaxy <superwhiteman@gmail.com>
2022-04-27 02:13:29 +0100

    test

--- module/s3/bucket-policy.tf
+++ module/s3/bucket-policy.tf
@@ -1,36 +1,64 @@
+# resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+#   bucket = aws_s3_bucket.versioning_bucket[0].id
+#   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+# }
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.versioning_bucket[0].id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+  policy = <<POLICY
+
+  {
+  "Id": "HTTPSPolicy",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowSSLRequestsOnly",
+      "Action": "s3:*",
+      "Effect": "Deny",
+      "Resource": [
+        "aws_s3_bucket.versioning_bucket[0].arn",
+        "aws_s3_bucket.versioning_bucket[0].arn/*"
+      ],
+      "Condition": {
+        "Bool": {
+          "aws:SecureTransport": "false"
+        }
+      },
+      "Principal": "arn:aws:iam::607281769355:root"
+    }
+  ]
 }
 
-data "aws_iam_policy_document" "allow_access_from_another_account" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::607281769355:root"]
-    }
+ POLICY
+}
 
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
-    ]
+# data "aws_iam_policy_document" "allow_access_from_another_account" {
+#   statement {
+#     principals {
+#       type        = "AWS"
+#       identifiers = ["arn:aws:iam::607281769355:root"]
+#     }
 
-    resources = [
-      aws_s3_bucket.versioning_bucket[0].arn,
-      "${aws_s3_bucket.versioning_bucket[0].arn}/*",
-    ]
-       condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
+#     actions = [
+#       "s3:GetObject",
+#       "s3:ListBucket",
+#     ]
 
-      values = [
-        "false"    
-      ]
-    }
+#     resources = [
+#       aws_s3_bucket.versioning_bucket[0].arn,
+#       "${aws_s3_bucket.versioning_bucket[0].arn}/*",
+#     ]
+#        condition {
+#       test     = "Bool"
+#       variable = "aws:SecureTransport"
+
+#       values = [
+#         "false"    
+#       ]
+#     }
       
 
 
 
 
-  }
-}
+#   }
+# }
